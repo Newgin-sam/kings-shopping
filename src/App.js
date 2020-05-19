b826d2981291949a2c148b792a104531ab7f08a0
Newgin-sam <newginsam@gmail.com>
2020-05-19 20:00:49 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import {Switch,Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
-
+import Header from './components/header/header.componenet';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/shop' component={ShopPage}/>
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop' component={ShopPage}/>
+      </Switch>
+    </div>
   );
 }
 
